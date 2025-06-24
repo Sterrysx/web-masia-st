c94ff0378d9aeb338e56e38890988c4a3bc3eef5
Sterry <sterryfv@gmail.com>
2025-06-24 17:39:15 +0200

    new changes

--- frontend/src/services/apiService.js
+++
@@ -1,7 +0,0 @@
-// services/apiService.js
-// Archivo de ejemplo para gestionar llamadas a la API
-
-export const fetchData = async () => {
-    // Implementar lógica de llamada a la API
-    return [];
-};
